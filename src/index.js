290ef0162e705f290dea449bf9c03abf4833aaf6
Paola <paola.obreguillermo@yahoo.com>
2019-07-11 22:51:48 -0500

    Folders almost done

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+import App from './App';
+
+
+
+//renders the App.js file to the root element (index.html in public folder)
+
+ReactDOM.render(<App />, document.getElementById("root"));
